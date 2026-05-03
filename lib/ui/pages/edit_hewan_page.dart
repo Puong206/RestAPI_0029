db078847cf565aa0bd68876629c482f633cb2d09
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-03 16:01:10 +0700

    feat: tambahkan kontroler teks untuk mengelola input di halaman EditHewanPage

--- lib/ui/pages/edit_hewan_page.dart
+++ lib/ui/pages/edit_hewan_page.dart
@@ -9,5 +9,10 @@ class EditHewanPage extends StatefulWidget {
 }
 
 class _EditHewanPageState extends State<EditHewanPage> {
-  
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _namaController;
+  late final TextEditingController _jenisController;
+  late final TextEditingController _tanggalController;
+  late final TextEditingController _hargaController;
+  late final TextEditingController _statusController;
 }
