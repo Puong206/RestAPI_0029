c3c2b35d563d33d6ffc7bb82200638c440a7e74e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-05-03 15:32:10 +0700

    feat: tambahkan fungsi untuk mengirim data hewan di halaman AddHewanPage

--- lib/ui/pages/add_hewan_page.dart
+++ lib/ui/pages/add_hewan_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restapi_0029/logic/bloc/hewan/hewan_bloc.dart';
+import 'package:restapi_0029/logic/bloc/hewan/hewan_event.dart';
 
 class AddHewanPage extends StatefulWidget {
   const AddHewanPage({super.key});
@@ -23,5 +26,18 @@ class _AddHewanPageState extends State<AddHewanPage> {
     _hargaController.dispose();
     _statusController.dispose();
     super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      final data = {
+        'nama': _namaController.text.trim(),
+        'jenis': _jenisController.text.trim(),
+        'tanggal_lahir': _tanggalLahirController.text.trim(),
+        'harga': int.tryParse(_hargaController.text.trim()) ?? 0,
+        'status': _statusController.text.trim()
+      };
+      context.read<HewanBloc>().add(CreateHewan(data));
+    }
   }
 }
